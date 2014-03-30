e2a5a4aee8311ab0279916a73deb0c0259ac3386
Dave Reed <dave256@mac.com>
2014-03-30 14:16:42 -0400

    Initial Commit

---
+++ DDRCoreData/DDRCoreData.h
@@ -0,0 +1,13 @@
+//
+//  DDRCoreData.h
+//  DDRCoreData
+//
+//  Created by David Reed on 3/30/14.
+//  Copyright (c) 2014 David Reed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DDRCoreData : NSObject
+
+@end
---
+++ DDRCoreData/DDRCoreData.m
@@ -0,0 +1,13 @@
+//
+//  DDRCoreData.m
+//  DDRCoreData
+//
+//  Created by David Reed on 3/30/14.
+//  Copyright (c) 2014 David Reed. All rights reserved.
+//
+
+#import "DDRCoreData.h"
+
+@implementation DDRCoreData
+
+@end
